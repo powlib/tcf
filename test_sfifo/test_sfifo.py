aa4e2437a38025b8589e011a3f6e499e2e66811e
Andrew Powell <andrewandrepowell2@gmail.com>
2018-08-12 20:11:27 -0500

    Been redoing the synchronous FIFO tests. It's near completion.

--- test_sfifo/test_sfifo.py
+++ test_sfifo/test_sfifo.py
@@ -1,70 +1,139 @@
-from cocotb          import test, coroutine, fork
-from cocotb.result   import TestFailure, TestSuccess, ReturnValue
-from powlib          import Transaction
-from powlib.drivers  import SfifoDriver
-from powlib.utils    import TestEnvironment
-from random          import randint
+from cocotb                              import test, coroutine
+from cocotb.log                          import SimLog
+from cocotb.result                       import ReturnValue, TestFailure
+from cocotb.triggers                     import Timer, FallingEdge
+from powlib                              import Namespace, Interface, Transaction
+from powlib.verify.agents.SystemAgent    import ClockDriver, ResetDriver
+from powlib.verify.agents.HandshakeAgent import HandshakeWriteDriver, HandshakeReadDriver, HandshakeMonitor, \
+                                                CoinTossAllow, AlwaysAllow, NeverAllow
+from powlib.verify.agents.RegisterAgent  import RegisterInterface
+from powlib.verify.blocks                import SwissBlock, ScoreBlock, AssertBlock, SucceedBlock, SourceBlock
+from random                              import randint
+
+class CountBlock(SwissBlock):
+    '''
+    Increments upon an input. Returns true once
+    the count reach the total.
+    '''
+
+    def __init__(self, total, inputs):
+        SwissBlock.__init__(self=self, trans_func=self._count_func, inputs=inputs)
+        self.__total = total
+        self.__count = 0
+
+    def _count_func(self, *ignore):
+        self.__count += 1
+        return self.__count==self.__total
 
 @coroutine
-def perform_setup(dut):
+def perform_setup(dut, total):
     '''
-    Prepares the test environment.
+    Generates the testbench environment.
     '''
 
-    # Create the test environment.
-    te = TestEnvironment(dut=dut.dut, name="testbench")
+    T = 3 # Total number of duts.
 
-    # Add the clocks and resets.
-    te._add_clock(clock=te.dut.clk, period=(5,"ns"))
-    te._add_reset(reset=te.dut.rst, associated_clock=te.dut.clk)
+    # Create source block.
+    srcblk    = SourceBlock()    
 
-    # Add the synchronous fifo driver to the environment.
-    te.sfd=SfifoDriver(entity=te.dut, clock=te.dut.clk)
+    # Create simulation end blocks.
+    failblk   = AssertBlock()
+    passblk   = SucceedBlock()
+    cntblk    = CountBlock(total=total, inputs=T+1)
 
-    # Start the environment.
-    yield te.start()    
+    # Connect the source block, count block, and pass block.
+    srcblk.outport.connect(cntblk.inports(0)).outport.connect(passblk.inport)
 
-    # Return the test environment.
+    # Create the agents for each dut.    
+    rst_dict = {}
+    rstparams_dict = {}
+    clk_dict = {}
+    clkparams_dict = {}
+    for each_agent in range(T):
+
+        # Get sim handle of fifo.
+        fifo   = getattr(dut,"dut{}".format(each_agent))
+
+        # Construct the components.
+        wrdrv = HandshakeWriteDriver(interface=Interface(clk=fifo.clk,
+                                                         rst=fifo.rst,
+                                                         vld=fifo.wrvld,
+                                                         rdy=fifo.wrrdy,
+                                                         data=fifo.wrdata))
+        wrdatamon = HandshakeMonitor(interface=Interface(clk=fifo.clk,
+                                                         rst=fifo.rst,
+                                                         vld=fifo.wrvld,
+                                                         rdy=fifo.wrrdy,
+                                                         data=fifo.wrdata))
+        wrnfmon = HandshakeMonitor(interface=Interface(clk=fifo.clk,
+                                                       rst=fifo.rst,
+                                                       vld=fifo.wrvld,
+                                                       rdy=fifo.wrrdy,
+                                                       nf=fifo.wrnf))        
+        rddrv = HandshakeReadDriver(interface=Interface(clk=fifo.clk,
+                                                        rst=fifo.rst,
+                                                        vld=fifo.rdvld,
+                                                        rdy=fifo.rdrdy,
+                                                        data=fifo.rddata))        
+        rdmon = HandshakeMonitor(interface=Interface(clk=fifo.clk,
+                                                     rst=fifo.rst,
+                                                     vld=fifo.rdvld,
+                                                     rdy=fifo.rdrdy,
+                                                     data=fifo.rddata))
+
+        # Create the scoreboards.
+        datscrblk = ScoreBlock(name="dut{}.data".format(each_agent))
+
+        # Connect the source block to driver.
+        srcblk.outport.connect(wrdrv.inport)
+
+        # Connect the scoreboard.
+        wrdatamon.outport.connect(datscrblk.inports(0)).outport.connect(failblk.inport)
+        rdmon.outport.connect(datscrblk.inports(1))
+
+        # Connect the read monitor to the count block as well.
+        rdmon.outport.connect(cntblk.inports(1+each_agent))
+
+        # Pack the reset and clock.
+        rst_name                 = "dut{}rst".format(each_agent)
+        clk_name                 = "dut{}clk".format(each_agent)
+        rst_dict[rst_name]       = fifo.rst
+        rstparams_dict[rst_name] = Namespace(associated_clock=fifo.clk)
+        clk_dict[clk_name]       = fifo.clk        
+        clkparams_dict[clk_name] = Namespace(period=(randint(2,8),"ns"))
+
+    # Generate the system agents.
+    clkdrv = ClockDriver(interface=Interface(**clk_dict),
+                         param_namespace=Namespace(**clkparams_dict))
+    rstdrv = ResetDriver(interface=Interface(**rst_dict),
+                         param_namespace=Namespace(**rstparams_dict))
+
+    # Create the testbench environment namespace.
+    te = Namespace(source=srcblk)
+
+    # Yield until all reset have been de-asserted.
+    yield rstdrv.wait()
+
+    # Returnthe testbench environment for the tests.
     raise ReturnValue(te)
 
+
 @test()
-def test_fifo(dut):
+def test_basic(dut):
+    '''
+    Simply write random values into the synchronous fifos.
+    '''
 
-    te = yield perform_setup(dut)
+    # Generate the testbench environment.
+    total     = 256
+    max_value = (1<<32)-1
+    te        = yield perform_setup(dut, total)
+    
+    # Write out the data.
+    for _ in range(total): te.source.write(data=Transaction(data=randint(0,max_value)))
 
-    width = te.sfd.W
-    total = 1<<width
-    depth = te.sfd.D
-    size  = depth*4
-    data  = lambda : randint(0, total-1)
-    exps  = []
-    acts  = []
-
-    # Write the data.    
-    exps.extend([data() for _ in range(size)])
-    for exp in exps: te.sfd.append(Transaction(wrdata=exp))    
-    te.sfd.append(Transaction())
-
-    # Wait some amount of clock cycles.
-    yield te.sfd.cycle(amount=depth+4)
-
-    # Read a bunch of data.
-    act = yield te.sfd.read(rdrdy=1)
-    acts.append(act)
-    for _ in range(size-1): 
-        act = yield te.sfd.read()
-        acts.append(act)    
-    yield te.sfd.read(rdrdy=0)
-
-    # Verify data.
-    for act, exp in zip(acts,exps):
-        te.log.info("Act: {}, Exp: {}".format(act, exp))
-        if act!=exp: raise TestFailure()
-
-    raise TestSuccess()
-
-
-
-
-
+    # If something goes wrong, this test can timeout.
+    yield Timer(1000, "us")
+    te.log.info("Timed out!")
+    raise TestFailure()       
 
--- test_sfifo/test_sfifo.v
+++ test_sfifo/test_sfifo.v
@@ -4,9 +4,13 @@ module test_sfifo();
 
   initial begin
     $dumpfile("waveform.vcd");
-    $dumpvars(2, dut);
+    $dumpvars(3, dut0);
+    $dumpvars(3, dut1);
+    $dumpvars(3, dut2);
   end  
 
-  powlib_sfifo #(.W(16),.D(8),.EDBG(1)) dut ();       
+  powlib_sfifo #(.W(16),.D(8), .EDBG(0),.NFS(0)) dut0 ();       
+  powlib_sfifo #(.W(16),.D(10),.EDBG(0),.NFS(4)) dut1 ();       
+  powlib_sfifo #(.W(16),.D(15),.EDBG(0),.NFS(2)) dut2 ();       
 
 endmodule
