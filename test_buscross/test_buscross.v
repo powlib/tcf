d9eb83c4ee5ce10544a41c26d55427355aa1aa26
Andrew Powell <andrewandrepowell2@gmail.com>
2018-08-22 00:29:26 -0500

    Well, now I got to write up the testbench. That's a work in progress.

--- test_buscross/test_buscross.v
+++ test_buscross/test_buscross.v
@@ -4,39 +4,56 @@ module test_buscross();
 
   initial begin
     $dumpfile("waveform.vcd");
-    $dumpvars(3, dut0);
-    $dumpvars(3, dut1);
-    $dumpvars(3, dut2);
+    $dumpvars(3, dut0);    
   end  
 
+
+  test_buscross_dut #(.ID("BUS0")) dut0();
 
 endmodule
 
 module test_buscross_dut();
 
-  wire [B_WRS-1:0]      wrclks;
-  wire [B_WRS-1:0]      wrrsts;
-  wire [B_DW-1:0]       wrdatas [0:B_WRS-1];
-  wire [B_AW-1:0]       wraddrs [0:B_WRS-1];
-  wire [B_WRS-1:0]      wrvlds;
-  wire [B_WRS-1:0]      wrrdys;
+  parameter                   ID        = "BUSCROSS";
+  parameter                   D         = 8;
+  parameter                   S         = 0;
+  parameter                   EDBG      = 0;
+  parameter                   B_AW      = 16;
+  parameter                   B_DW      = 32;
+  parameter                   B_WRS     = 3;
+  parameter                   B_RDS     = 4;
+  parameter [B_RDS*B_AW-1:0]  B_BASES   = {16'h0000,16'h2000,16'h4000,16'h0000};
+  parameter [B_RDS*B_AW-1:0]  B_SIZES   = {16'h1FFF,16'h1FFF,16'h1FFF,16'hFFFF};
+  parameter [B_WRS*B_RDS-1:0] B_EASYNCS = {3'b000,3'b000,3'b000,3'b000};
+  parameter                   B_D       = 8;
+  parameter                   B_DD      = 4;
+  parameter                   B_S       = 0;
 
-  wire [B_RDS-1:0]      rdclks;
-  wire [B_RDS-1:0]      rdrsts;
-  wire [B_DW-1:0]       rddatas [0:B_RDS-1];
-  wire [B_AW-1:0]       rdaddrs [0:B_RDS-1];
-  wire [B_RDS-1:0]      rdvlds;
-  wire [B_RDS-1:0]      rdrdys;  
+  wire      [B_WRS-1:0]       wrclks;
+  wire      [B_WRS-1:0]       wrrsts;
+  wire      [B_DW-1:0]        wrdatas [0:B_WRS-1];
+  wire      [B_AW-1:0]        wraddrs [0:B_WRS-1];
+  wire      [B_WRS-1:0]       wrvlds;
+  wire      [B_WRS-1:0]       wrrdys;
+      
+  wire      [B_RDS-1:0]       rdclks;
+  wire      [B_RDS-1:0]       rdrsts;
+  wire      [B_DW-1:0]        rddatas [0:B_RDS-1];
+  wire      [B_AW-1:0]        rdaddrs [0:B_RDS-1];
+  wire      [B_RDS-1:0]       rdvlds;
+  wire      [B_RDS-1:0]       rdrdys;  
+      
+  wire      [B_WRS*B_DW-1:0]  datas_s0_0;
+  wire      [B_WRS*B_AW-1:0]  addrs_s0_0;
+  wire      [B_WRS-1:0]       vlds_s0_0;
+  wire      [B_WRS-1:0]       rdys_s0_0;
+  wire      [B_WRS-1:0]       nfs_s0_0;
+  wire      [B_RDS*B_DW-1:0]  datas_s1_0;
+  wire      [B_RDS*B_AW-1:0]  addrs_s1_0;
+  wire      [B_RDS-1:0]       vlds_s1_0;
+  wire      [B_RDS-1:0]       rdys_s1_0;  
 
-  wire [B_WRS*B_DW-1:0] datas_s0_0;
-  wire [B_WRS*B_AW-1:0] addrs_s0_0;
-  wire [B_WRS-1:0]      vlds_s0_0;
-  wire [B_WRS-1:0]      rdys_s0_0;
-  wire [B_WRS-1:0]      nfs_s0_0;
-  wire [B_RDS*B_DW-1:0] datas_s1_0;
-  wire [B_RDS*B_AW-1:0] addrs_s1_0;
-  wire [B_RDS-1:0]      vlds_s1_0;
-  wire [B_RDS-1:0]      rdys_s1_0;  
+  genvar                      i, j;
 
   for (i=0; i<B_WRS; i=i+1) begin
     assign datas_s0_0[i*B_DW+:B_DW] = wrdatas[i];
