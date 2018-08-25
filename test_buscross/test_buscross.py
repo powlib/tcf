d86a1ee7c530432fbbecc71bc7f5555682a77959
Andrew Powell <andrewandrepowell2@gmail.com>
2018-08-25 17:11:04 -0500

    Nearly there. Just need to set up the testbench such that it compares sets, rather than order.

--- test_buscross/test_buscross.py
+++ test_buscross/test_buscross.py
@@ -8,9 +8,114 @@ from powlib.verify.agents.SystemAgent    import ClockDriver, ResetDriver
 from powlib.verify.agents.HandshakeAgent import HandshakeWriteDriver, HandshakeReadDriver, HandshakeMonitor, \
                                                 CoinTossAllow, AlwaysAllow, NeverAllow
 from powlib.verify.agents.RegisterAgent  import RegisterInterface
-from powlib.verify.blocks                import SwissBlock, ScoreBlock, AssertBlock, SucceedBlock, SourceBlock, AnyCondFunc
+from powlib.verify.block                 import Block, InPort, OutPort
+from powlib.verify.blocks                import SwissBlock, ScoreBlock, AssertBlock, SucceedBlock, SourceBlock, \
+                                                ComposeBlocks, ComposedBlock, PrintBlock
 from random                              import randint
 from itertools                           import product
+
+def ChangeWidth(trans,aw,dw):
+    '''
+    Creates a new transaction whose
+    address and data fields are masked
+    according to the specified widths.
+    '''
+    awm = (1<<aw)-1
+    dwm = (1<<dw)-1
+    return Transaction(addr=trans.addr&awm,
+                       data=trans.data&dwm)
+
+class GatherBlock(Block):
+    '''
+    This gather block was created such that sets of
+    data can be compared with the scoreboard.
+    '''
+
+    def __init__(self):
+        self.__inport = InPort(block=self)
+        self.__outport = OutPort(block=self)
+        self.__lst = []
+
+    inport  = property(lambda self : self.__inport)        
+    outport = property(lambda self : self.__outport)
+
+    def _behavior(self):
+        if self.__inport.ready():
+            self.__lst.append(self.__inport.read())
+
+    def perform(self):
+        self.__outport.write(data=set(self.__lst))            
+        self.__lst = []
+
+
+class BusCrossBlock(Block):
+    '''
+    Models the behavior of the crossbar.
+    '''
+
+    def __init__(self, inputs, outputs, outport_params):
+        '''
+        Constructs the crossbar model.
+        inputs         = Number of inports that represent the writing interfaces.
+        outputs        = Number of outports that represent the reading interfaces.
+        outport_params = List of Namespaces that correspond to the outputs. 
+                         Each Namespace should contain a base address and
+                        size, where size is minus 1 the actual size.
+        '''
+
+        # Create the inports.
+        self.__inports = []
+        for each_inport in range(inputs):
+            self.__inports.append(InPort(block=self))
+
+        # Create the outports.
+        self.__outports       = []
+        self.__outport_params = outport_params
+        for each_outport in range(outputs):
+            self.__outports.append(OutPort(block=self))
+            outport_param = outport_params[each_outport]
+            if not hasattr(outport_param,"base"):
+                raise ValueError("{} outport should have member base.".format(each_outport)) 
+            if not hasattr(outport_param,"size"):
+                raise ValueError("{} outport should have member size.".format(each_outport))                 
+
+    # Returns the sizes of the inports and outports.
+    inputs  = property(lambda self : len(self.__inports))                
+    outputs = property(lambda self : len(self.__outports))                
+
+    def inports(self, idx):
+        '''
+        Returns the specified inport.
+        '''                
+        return self.__inports[idx]
+
+    def outports(self, idx):
+        '''
+        Returns the specified outport.
+        '''        
+        return self.__outports[idx]
+
+    def outport_params(self, idx):
+        '''
+        Returns the specified outport param.
+        '''        
+        return self.__outport_params[idx]
+
+    def _behavior(self):
+        '''
+        Defines the behavior of the crossbar model.
+        '''      
+
+        # Behavior is straight forward. Simply write out
+        # the transaction to all outports whose address range
+        # contains the specified inport address.      
+        for inport in self.__inports:
+            if inport.ready():
+                trans = inport.read()
+                for param, outport in zip(self.__outport_params,self.__outports):
+                    if trans.addr in range(param.base,param.base+param.size+1):
+                        outport.write(data=trans)
+
 
 @coroutine
 def perform_setup(dut):
@@ -19,6 +124,8 @@ def perform_setup(dut):
 
     # The following structures need to be created
     # prior to another initialization.
+    sizes_dict   = {}
+    src_blk_dict = {}
     rst_sig_dict = {}
     rst_prm_dict = {}
     clk_sig_dict = {}
@@ -28,10 +135,26 @@ def perform_setup(dut):
     for each_dut in range(TDUTS):
 
         # Get SimHandleBase of the dut and necessary parameters.. 
-        bus      = getattr(dut,"dut{}".format(each_dut)) # SimHandleBase
-        bwrs     = int(bus.B_WRS.value)                  # Total write interfaces
-        brds     = int(bus.B_RDS.value)                  # Total read interfaces
-        bclkprdw = int(bus.B_CLKPRDW.value)              # Gets the clock period width.
+        bus          = getattr(dut,"dut{}".format(each_dut)) # SimHandleBase
+        bwrs         = int(bus.B_WRS.value)                  # Total write interfaces
+        brds         = int(bus.B_RDS.value)                  # Total read interfaces
+        bclkprdw     = int(bus.B_CLKPRDW.value)              # Gets the clock period width.
+        baw          = int(bus.B_AW.value)                   # Address width
+        bwd          = int(bus.B_DW.value)                   # Data width     
+        ast_blk      = AssertBlock()                         # Used for failing test if hardware doesn't match hardware. 
+
+        sizes_dict[(each_dut,"wr")] = bwrs
+        sizes_dict[(each_dut,"rd")] = brds          
+
+        # Create the bus model.
+        out_prms_lst = []  
+        for idx in range(brds):
+            bases = int(bus.B_BASES.value)
+            sizes = int(bus.B_SIZES.value)
+            base  = (bases>>(baw*idx))&((1<<baw)-1)
+            size  = (sizes>>(baw*idx))&((1<<baw)-1)
+            out_prms_lst.append(Namespace(base=base,size=size))   
+        mdl_blk = BusCrossBlock(inputs=bwrs,outputs=brds,outport_params=out_prms_lst)                       
 
         # Pack clock/resets and create agents.        
         synclst =      [("wr",each_sync) for each_sync in range(bwrs)]
@@ -70,6 +193,38 @@ def perform_setup(dut):
             clk_sig_dict[clk_nme] = clk_sig
             clk_prm_dict[clk_nme] = Namespace(period=(prdval,"ns"))
 
+            # Operations associated with only the writing interface.
+            if intr=="wr":
+
+                # Create blocks.                
+                src_blk = SourceBlock()
+                msk_blk = SwissBlock(trans_func=lambda trans : ChangeWidth(trans=trans,aw=baw,dw=bwd))
+                gat_blk = GatherBlock()
+                ComposeBlocks(src_blk,msk_blk,drv)
+                msk_blk.outport.connect(mdl_blk.inports(idx))
+                msk_blk.outport.connect(PrintBlock("bus{}.wr{}".format(each_dut,idx)).inport)
+
+                # Store the source blocks.
+                src_blk_dict[(each_dut,idx)] = src_blk
+
+            # Operations associated with only the reading interface.
+            if intr=="rd":
+
+                # Store the base and size parmateres.
+                bases = int(bus.B_BASES.value)
+                sizes = int(bus.B_SIZES.value)
+                base  = (bases>>(baw*idx))&((1<<baw)-1)
+                size  = (sizes>>(baw*idx))&((1<<baw)-1)
+                out_prms_lst.append(Namespace(base=base,size=size)) 
+
+                # Create a scoreboard for each reading interface.
+                sb_blk = ScoreBlock(name="bus{}.rd{}".format(each_dut,idx))
+                mdl_blk.outports(idx).connect(sb_blk.inports(0))
+                mon.outport.connect(sb_blk.inports(1))                
+                ComposeBlocks(sb_blk,ast_blk)
+                mon.outport.connect(PrintBlock("bus{}.rd{}".format(each_dut,idx)).inport)
+                  
+
     # Generate the system agents.
     clk_drv = ClockDriver(interface=Interface(**clk_sig_dict),
                           param_namespace=Namespace(**clk_prm_dict))
@@ -80,7 +235,10 @@ def perform_setup(dut):
     yield rst_drv.wait()                                  
 
     # Create testbench environment namespace.
-    te = Namespace()
+    te = Namespace(sources   = lambda bus, idx : src_blk_dict[(bus,idx)],
+                   buses     = TDUTS,
+                   inputs    = lambda bus : sizes_dict[(bus,"wr")],
+                   outputs   = lambda bus : sizes_dict[(bus,"rd")])
 
     raise ReturnValue(te)
 
@@ -93,5 +251,14 @@ def test_basic(dut):
 
     te = yield perform_setup(dut)
 
-    yield Timer(100,"ns")
+    rword  = lambda : randint(0,(1<<32)-1)
+    writes = 2
+
+    for each_write in range(writes):
+        for each_bus in range(te.buses):
+            for each_source in range(te.inputs(each_bus)):
+                source = te.sources(bus=each_bus,idx=each_source)
+                source.write(data=Transaction(addr=rword(),data=rword()))
+
+    yield Timer(1000,"ns")
     pass 
